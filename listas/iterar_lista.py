fc7ac0e6bb863ee8bb7169618b348b9eeb26ff6f
victorcuervo <vcuervo@gmail.com>
2015-11-16 00:57:54 +0100

    Iterar una lista en Python

---
+++ listas/iterar_lista.py
@@ -0,0 +1,9 @@
+#Definimos la lista
+lista = ["carmen","elena","lucia","sara","patricia","sonsoles"]
+
+# La recorremos con un foreach
+print "Recorrer lista con un foreach"
+for elemento in lista:
+    print elemento
+
+
