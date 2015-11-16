ab58ede052ceda6ae9a645a489e07d14399349d2
victorcuervo <vcuervo@gmail.com>
2015-11-17 00:07:18 +0100

    Listar recursivas en Python

---
+++ listas/lista_recursiva.py
@@ -0,0 +1,11 @@
+#Codigo para recorrer listas de forma recursiva
+
+def listar (lista):
+    for n in lista:
+        if isinstance(n,list):
+            listar(n)
+        else:
+            print n
+
+lista = [1,2,3,['a','b','c','d'],4,5]
+listar(lista)
