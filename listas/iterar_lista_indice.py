71e0e46dd73a488106e0716e55cf7f7989a887f7
victorcuervo <vcuervo@gmail.com>
2015-11-16 01:16:45 +0100

    Iterar una lista en Python con indices

---
+++ listas/iterar_lista_indice.py
@@ -0,0 +1,7 @@
+#Definimos la lista
+lista = ["carmen","elena","lucia","sara","patricia","sonsoles"]
+
+# Mediante indices
+print "Recorrer lista por Indices"
+for x in range(0,len(lista)):
+    print lista[x]
