fe62bc6e446c752ecb6647e00e83c5eeff3feedc
victorcuervo <vcuervo@gmail.com>
2016-03-06 00:26:48 +0100

    Borrar un fichero de Amazon S3

---
+++ tinys3/borrar_fichero_s3.py
@@ -0,0 +1,16 @@
+#coding=utf-8
+import tinys3
+
+S3_ACCESS_KEY = 'BAKIBAKI678H67HGA'
+S3_SECRET_KEY = '+vpOpILD+E9872AialendX0Ui123CKCKCKw'
+BUCKET = '/vcp-prueba'
+
+conn = tinys3.Connection(S3_ACCESS_KEY,S3_SECRET_KEY,'vcp-prueba',endpoint='s3-eu-west-1.amazonaws.com')
+
+print '-- Borrado Fichero S3 --'
+
+filename = 'fichero.png'
+conn.delete(filename,BUCKET)
+print 'Borrado el fichero '+filename
+
+print '-- Fin Borrado S3 --'
