7b80b7d6b3c49aee9bf5effb315ccaad0d9ac526
victorcuervo <vcuervo@gmail.com>
2016-03-07 01:46:10 +0100

    Crear un cursor en Pymongo

---
+++ pymongo/cursor-pymongo.py
@@ -0,0 +1,19 @@
+#encoding=utf-8
+
+from pymongo import MongoClient
+
+# Conectamos a MongoDB
+client = MongoClient()
+
+# Seleccionamos la base de datos
+db = client.users
+
+# Seleccionamos la colección
+listado = db.listado
+
+# Imprimimos el primer resultado
+usuarios = listado.find()
+
+for usuario in usuarios:
+    print usuario
+
