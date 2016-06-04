531c596d7e121186e0600f13639c3de7c6a9d76a
victorcuervo <vcuervo@gmail.com>
2016-06-04 20:53:52 +0200

    Actualizar un documento con Pymongo.

---
+++ pymongo/update-pymongo.py
@@ -0,0 +1,23 @@
+#encoding=utf-8
+from pymongo import MongoClient
+
+# Conectamos a MongoDB
+client = MongoClient()
+
+# Seleccionamos la base de datos
+db = client.test
+
+# Seleccionamos la colección
+users = db.users
+
+
+# Documento a buscar para actualizar
+searchuser = {
+    'nombre': 'Pilar',
+    'edad': 30
+}
+
+# Insertamos el documento
+resultado = users.update_many(searchuser,{"$set": {"edad":31}})
+
+print 'Numero de documentos modificados ' + str(resultado.modified_count)
