268b0bfab41038a79e5855a6b57e853c963b5317
victorcuervo <vcuervo@gmail.com>
2017-06-11 23:26:14 +0200

    Lista aleatoria.

---
+++ matematicos/lista_aleatoria.py
@@ -0,0 +1,8 @@
+# encoding=utf-8
+from random import sample
+
+lista = range(0,100)
+print sample(lista,k=10)
+
+vehiculos = ['avión','bicicleta','coche','motocicleta','tractor','barco','transatlántico','camión']
+print sample(vehiculos,k=4)
