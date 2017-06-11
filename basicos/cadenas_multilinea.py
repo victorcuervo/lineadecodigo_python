fe3f672302a37ae4878d6bbdd9cdc9b18567813f
victorcuervo <vcuervo@gmail.com>
2017-06-11 23:47:41 +0200

    Multi-líneas con Python

---
+++ basicos/cadenas_multilinea.py
@@ -0,0 +1,8 @@
+# encoding=utf-8
+# Ejemplo de cadenas multilínea
+
+texto = "En un lugar de la Mancha, de cuyo nombre no quiero acordarme, "\
+        "no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero, "\
+        "adarga antigua, rocín flaco y galgo corredor."
+
+print texto
