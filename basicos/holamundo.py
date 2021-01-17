6e0043736f6f637d630ea60b2923e1ab296d928f
victorcuervo <vcuervo@gmail.com>
2021-01-17 11:54:53 +0100

    Añadidos comentarios

--- basicos/holamundo.py
+++ basicos/holamundo.py
@@ -1,2 +1,10 @@
-#Hola Mundo en Python
-print "Hola Mundo!!!"
+# ##
+# @file holamundo.py
+# @version 1.0
+# @author Víctor Cuervo - http://lineadecodigo.com
+# @date   15/noviembre/2015
+# @url  http://lineadecodigo.com/python/hola-mundo-en-python/
+# @description Hola Mundo en Python
+# ##
+
+print ("Hola Mundo!!!")
