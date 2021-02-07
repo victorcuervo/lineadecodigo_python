8ce20e809db675e1e90b638e646900041bc01498
victorcuervo <vcuervo@gmail.com>
2021-02-07 02:06:28 +0100

    Elimina elementos duplicados de una lista

---
+++ listas/eliminar-elementos-duplicados-lista.py
@@ -0,0 +1,18 @@
+# ##
+# @file eliminar-elemento-listapy
+# @version 1.0
+# @author Víctor Cuervo - http://lineadecodigo.com
+# @date   7/febrero/2021
+# @url  http://lineadecodigo.com/python/pdte/
+# @description Elimina los elementos duplicados de una lista
+# ##
+
+
+# Codigo que nos permite add un elemento a una lista
+
+lista = [1,4,3,4,5,1,2,8,7,4,3,1,3]
+print (lista)
+
+# Al crear un diccionario elimina los duplicados
+lista = list(dict.fromkeys(lista))
+print (lista)
