e7fea1ff5c88d3768d57c9e58ce8cc4f2ec24782
victorcuervo <vcuervo@gmail.com>
2021-02-27 22:45:26 +0100

    Artículo publicado

--- listas/eliminar-elemento-lista.py
+++ listas/eliminar-elemento-lista.py
@@ -3,7 +3,7 @@
 # @version 1.0
 # @author Víctor Cuervo - http://lineadecodigo.com
 # @date   7/febrero/2021
-# @url  http://lineadecodigo.com/python/pdte/
+# @url  http://lineadecodigo.com/python/eliminar-elemento-de-una-lista-con-python/
 # @description Elimina un elemento de una lista
 # ##
 
