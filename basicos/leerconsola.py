9dbad29839c9766d6e016f6a0ee7c1afa69520d5
victorcuervo <vcuervo@gmail.com>
2021-10-13 13:51:10 +0200

    Incluida URL

--- basicos/leerconsola.py
+++ basicos/leerconsola.py
@@ -3,11 +3,11 @@
 # @version 1.0
 # @author Víctor Cuervo - http://lineadecodigo.com
 # @date   5/septiembre/2021
-# @url  http://lineadecodigo.com/python/xxx/
+# @url  http://lineadecodigo.com/python/leer-por-consola-en-python/
 # @description Pedir un dato por consola
 # ##
 
 print ("¿Cómo te llamas?")
-nombre = input();
+nombre = input()
 
 print ("Hola " + nombre + ", ¿Cómo estás?")
