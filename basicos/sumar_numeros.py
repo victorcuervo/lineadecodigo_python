9081541e7f83c3342b06add2de3e4acbb1dce0aa
victorcuervo <vcuervo@gmail.com>
2021-10-13 13:54:43 +0200

    sumar números en python

---
+++ basicos/sumar_numeros.py
@@ -0,0 +1,18 @@
+# ##
+# @file sumar_numeros.py
+# @version 1.0
+# @author Víctor Cuervo - http://lineadecodigo.com
+# @date   13/octubre/2021
+# @url  http://lineadecodigo.com/python/xxxx/
+# @description Sumar números por consola en Python
+# ##
+
+print ("¿Dime el primer número?")
+n1 = input()
+
+print ("¿Dime el segundo número?")
+n2 = input()
+
+suma = int(n1) + int(n2)
+
+print ("La suma de " + n1 + "+" + n2 + " es igual a " + str(suma))
