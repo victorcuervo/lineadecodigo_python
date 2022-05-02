0f086921a858cd4ec1fc21e56ec14c3a8f5601af
victorcuervo <vcuervo@gmail.com>
2022-05-02 13:13:05 +0200

    Añadida URL

--- json/cargar-json-url.py
+++ json/cargar-json-url.py
@@ -3,12 +3,14 @@
 # @version 1.0
 # @author Víctor Cuervo - http://lineadecodigo.com
 # @date   21/noviembre/2021
-# @url  http://lineadecodigo.com/python/xxx/
+# @url  http://lineadecodigo.com/python/cargar-json-desde-una-url-con-python/
 # @description Cargar contenido de un fichero JSON desde una URL
 # ##
 
 import requests
 import json
+
+print(requests.get("https://jsonplaceholder.typicode.com/todos/1"))
 
 response = requests.get("https://jsonplaceholder.typicode.com/todos/1").text
 objeto = json.loads(response)
