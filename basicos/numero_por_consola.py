25ff06106a1b7be5ca9f944dbf1c4f00fa297786
victorcuervo <vcuervo@gmail.com>
2022-05-12 12:13:24 +0200

    Controlar Número por consola en Python

---
+++ basicos/numero_por_consola.py
@@ -0,0 +1,19 @@
+# ##
+# @file numero_por_consola.py
+# @version 1.0
+# @author Víctor Cuervo - http://lineadecodigo.com
+# @date   12/mayo/2022
+# @url  http://lineadecodigo.com/python/xxxx/
+# @description Controlar que los datos de entrada son numéricos
+# ##
+
+while True:
+    try:
+        valor = int(input("Inserta un valor que sea un número: "))
+        break
+    except ValueError:
+        print("Lo que has introducido no es un número. Inténtalo de nuevo.")
+
+print ("Gracias! Has introducido el número " + str(valor))
+
+
