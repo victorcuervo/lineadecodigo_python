cde4bdc1a2cb1a02341a4d81b2e56f62a9b1288e
victorcuervo <vcuervo@gmail.com>
2022-05-18 09:44:16 +0200

    añadida URL

--- basicos/sumar_numeros.py
+++ basicos/sumar_numeros.py
@@ -3,16 +3,34 @@
 # @version 1.0
 # @author Víctor Cuervo - http://lineadecodigo.com
 # @date   13/octubre/2021
-# @url  http://lineadecodigo.com/python/xxxx/
+# @url  http://lineadecodigo.com/python/sumar-numeros-en-python/
 # @description Sumar números por consola en Python
 # ##
 
-print ("¿Dime el primer número?")
-n1 = input()
+# Programa sin controles
+print("Programa sin controles")
+n1 = input("¿Dime el primer número?")
+n2 = input("¿Dime el segundo número?")
 
-print ("¿Dime el segundo número?")
-n2 = input()
+# Suma de caracteres y no números
+print (n1+n2)
 
-suma = int(n1) + int(n2)
 
-print ("La suma de " + n1 + "+" + n2 + " es igual a " + str(suma))
+print("Programa con controles")
+# Controlar el error de conversión
+
+while True:
+    try:
+        n1 = int(input("¿Dime el primer número?"))
+        break
+    except ValueError:
+        print("El valor introducido no es un número. Intenta de nuevo")
+
+while True:
+    try:
+        n2 = int(input("¿Dime el segundo número?"))
+        break
+    except ValueError:
+        print("El valor introducido no es un número. Intenta de nuevo")
+
+print ("La suma de los dos números es: " + str(n1+n2))
