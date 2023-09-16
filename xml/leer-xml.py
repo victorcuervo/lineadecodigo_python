97e12ea1d59fb59e1d48544c83af31f246e04584
victorcuervo <vcuervo@gmail.com>
2023-09-16 22:41:22 +0200

    leer xml con python

---
+++ xml/leer-xml.py
@@ -0,0 +1,15 @@
+# ##
+# @file leer-xml.py
+# @version 1.0
+# @author Víctor Cuervo - http://lineadecodigo.com
+# @date   16/septiembre/2023
+# @url  http://lineadecodigo.com/python/pdte/
+# @description Leer un fichero XML con Python
+# ##
+
+import xml.etree.ElementTree as ET
+
+root = ET.parse("data.xml").getroot()
+
+for nombre in root.findall('nombre'):
+    print (nombre.text)
