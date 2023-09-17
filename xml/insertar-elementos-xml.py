a03f27eeca756626d0a9ac0a05e6fbf9f2cbb0d1
victorcuervo <vcuervo@gmail.com>
2023-09-17 20:57:03 +0200

    Insertar Elementos XML con Python

---
+++ xml/insertar-elementos-xml.py
@@ -0,0 +1,23 @@
+# ##
+# @file insertar-elementos-xml.py
+# @version 1.0
+# @author Víctor Cuervo - http://lineadecodigo.com
+# @date   16/septiembre/2023
+# @url  http://lineadecodigo.com/python/pdte/
+# @description Leer un fichero XML con Python
+# ##
+
+import xml.etree.ElementTree as ET
+
+tree = ET.parse("data.xml")
+root = tree.getroot()
+
+lista_nombres = ['Victor','Ignacio','Elena']
+
+for nombre in lista_nombres:
+    nombreElemento = ET.Element("nombre")
+    nombreElemento.text = nombre
+    root.insert(-1,nombreElemento)
+
+
+tree.write("data3.xml",encoding='utf-8',xml_declaration=True)
