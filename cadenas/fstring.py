87c927f34d363d960b1e365b55ecaaf94406b6d2
victorcuervo <vcuervo@gmail.com>
2023-10-06 00:11:20 +0200

    fstring en Python

---
+++ cadenas/fstring.py
@@ -0,0 +1,18 @@
+# ##
+# @file fstring.py
+# @version 1.0
+# @author Víctor Cuervo - http://lineadecodigo.com
+# @date   6/octubre/2023
+# @url  http://lineadecodigo.com/python/pdte/
+# @description Uso de f-string o interpolación de cadenas
+# ##
+
+
+a=2
+b=3
+cadena = f'La suma de {a} y {b} es {a+b}'
+print(cadena)
+
+nombre = 'Luis'
+saludo = f'¿Cómo estás {nombre}?'
+print (saludo)
