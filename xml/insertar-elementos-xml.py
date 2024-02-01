58f284822e410106212cc0cf2e9caa9a1f0ea22d
victorcuervo <vcuervo@gmail.com>
2024-02-01 23:09:25 +0100

    Añadida URL

--- xml/insertar-elementos-xml.py
+++ xml/insertar-elementos-xml.py
@@ -3,13 +3,13 @@
 # @version 1.0
 # @author Víctor Cuervo - http://lineadecodigo.com
 # @date   16/septiembre/2023
-# @url  http://lineadecodigo.com/python/pdte/
+# @url  http://lineadecodigo.com/python/actualizar-un-fichero-xml-con-python/
 # @description Leer un fichero XML con Python
 # ##
 
 import xml.etree.ElementTree as ET
 
-tree = ET.parse("data.xml")
+tree = ET.parse("xml/data.xml")
 root = tree.getroot()
 
 lista_nombres = ['Victor','Ignacio','Elena']
