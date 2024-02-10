8cd3bb3db0c36cf968733195ee8ade57bdd617ef
victorcuervo <vcuervo@gmail.com>
2024-02-10 16:11:12 +0100

    Bucle for

---
+++ basicos/listar-numeros-impares.py
@@ -0,0 +1,11 @@
+# ##
+# @file listar-numeros-impares.py
+# @version 1.0
+# @author Víctor Cuervo - http://lineadecodigo.com
+# @date   10/febrero/2024
+# @url  http://lineadecodigo.com/python/xxx/
+# @description Utilizar un bucle for para mostrar los números imparres del 1 al 10.
+# ##
+
+for numero in range(1,11,2):
+  print(numero)
---
+++ basicos/recorrer-numeros.py
@@ -0,0 +1,12 @@
+# ##
+# @file recorrer-numeros.py
+# @version 1.0
+# @author Víctor Cuervo - http://lineadecodigo.com
+# @date   10/febrero/2024
+# @url  http://lineadecodigo.com/python/xxx/
+# @description Utilizar un bucle for para mostrar los 10 primeros números
+# ##
+
+for numero in range(1,11):
+  print(numero)
+
